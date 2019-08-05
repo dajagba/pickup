fc249ee3e3257e8bf5e5639e3d7305959f6ebadf
Dominic Ajagba <dominicajagba@dominics-mbp.home>
2019-08-04 20:43:51 -0400

    Initial Pickup app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { Plugins } from '@capacitor/core';
+const { SplashScreen } = Plugins;
+//import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {timer} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  showSplash:boolean = true; 
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
+   // private SplashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
@@ -20,7 +24,12 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      SplashScreen.show({
+      showDuration: 1000,
+      autoHide: true
+});
+timer(6000).subscribe(()=> this.showSplash = false); 
+
     });
   }
 }
